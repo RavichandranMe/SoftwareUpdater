bf22c22a4b5f8b9673e074bc2fc4a2c00b88e5d9
RavichandranMe <127849390+RavichandranMe@users.noreply.github.com>
2024-04-17 14:43:14 +0530

    Changes

--- Sheduler2.ps1
+++ Sheduler2.ps1
@@ -1,5 +1,10 @@
-# Direct link to the raw content of the script on GitHub
-$scriptPath = "https://raw.githubusercontent.com/RavichandranMe/SoftwareUpdater/main/Developers.ps1"
+# Define the path to your PowerShell script
+$url = "https://raw.githubusercontent.com/RavichandranMe/SoftwareUpdater/main/Developers.ps1"
+$scriptPath = "C:\ProgramData\DevelopersNew.ps1"
+$outputLogFile = "C:\ProgramData\DevelopersNew.log"  # Define the path to the log file
+
+# Download the script
+Invoke-WebRequest -Uri $url -OutFile $scriptPath
 
 # Get the current hour
 $currentHour = (Get-Date).Hour
@@ -7,29 +12,14 @@ $currentHour = (Get-Date).Hour
 # Calculate the next run time based on the current hour
 $nextRunTime = (Get-Date).Date.AddDays(1).AddHours($currentHour)
 
-# Create a TimeSpan object for the repetition interval
-$repetitionInterval = New-TimeSpan -Hours 1
-
 # Create a new scheduled task trigger
-$trigger = New-ScheduledTaskTrigger -Once -At $nextRunTime -RepetitionInterval $repetitionInterval
+$trigger = New-ScheduledTaskTrigger -Once -At $nextRunTime -RepetitionInterval "01:00:00"
 
 # Define the action with elevated privileges
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $scriptPath -Verb runas -WindowStyle hidden"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $scriptPath -Verb runas -WindowStyle hidden > $outputLogFile"  # Redirect output to the log file
 
 # Register the scheduled task
-try {
-    Register-ScheduledTask -TaskName "Updater" -Trigger $trigger -Action $action -RunLevel Highest -ErrorAction Stop
+Register-ScheduledTask -TaskName "Updater" -Trigger $trigger -Action $action -RunLevel Highest
 
-    # Informative message
-    Write-Host "Successfully created a scheduled task to run '$scriptPath' every hour with admin privileges."
-}
-catch {
-    Write-Host "Failed to create the scheduled task: $_" -ForegroundColor Red
-}
-
-# Verbose output
-$VerbosePreference = "Continue"
-Write-Verbose "Script execution completed."
-
-# Pause to keep the window open
-Read-Host -Prompt "Press Enter to exit"
+# Informative message
+Write-Host "Successfully created a scheduled task to run '$scriptPath' every hour with admin privileges. Output will be logged to '$outputLogFile'."
